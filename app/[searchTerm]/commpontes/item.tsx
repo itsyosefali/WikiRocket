ee902fe6dbce633cd0432c0f37bfd1f6e3bc8f13
itsyosefali <joeyxjoey123@gmail.com>
2023-07-25 19:21:18 +0200

    fix the desing

--- app/[searchTerm]/commpontes/item.tsx
+++ app/[searchTerm]/commpontes/item.tsx
@@ -1,45 +1,44 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from 'next/image';
+import { Typography } from '@mui/material';
 
 type Props = {
-    result: Result
-}
+  result: Result;
+};
 
 export default function Item({ result }: Props) {
-    const itemTextCol = (
+  const itemTextCol = (
+    <div className="flex flex-col justify-center">
+      <Typography variant="h5" component="h2" className="text-xl font-bold underline">
+        <Link href={`https://en.wikipedia.org/?curid=${result.pageid}`} target="_blank">
+          {result.title}
+        </Link>
+      </Typography>
+      <Typography variant="body1">{result.extract}</Typography>
+    </div>
+  );
+
+  
+  const content = result?.thumbnail?.source ? (
+    <article className="m-4 max-w-lg">
+      <div className="flex flex-row gap-4">
         <div className="flex flex-col justify-center">
-            <h2>
-                <Link href={`https://en.wikipedia.org/?curid=${result.pageid}`} target="_blank"
-                    className="text-xl font-bold underline">
-                    {result.title}
-                </Link>
-            </h2>
-            <p>{result.extract}</p>
+          <Image
+            src={result.thumbnail.source}
+            alt={result.title}
+            width={result.thumbnail.width}
+            height={result.thumbnail.height}
+            loading="lazy"
+          />
         </div>
-    )
+        {itemTextCol}
+      </div>
+    </article>
+  ) : (
+    <article className="m-4 max-w-lg">
+      {itemTextCol}
+    </article>
+  );
 
-    const content = result?.thumbnail?.source
-        ? (
-            <article className="m-4 max-w-lg">
-                <div className="flex flex-row gap-4">
-                    <div className="flex flex-col justify-center">
-                        <img
-                            src={result.thumbnail.source}
-                            alt={result.title}
-                            width={result.thumbnail.width}
-                            height={result.thumbnail.height}
-                            loading="lazy"
-                        />
-                    </div>
-                    {itemTextCol}
-                </div>
-            </article>
-
-        )
-        : (
-            <article className="m-4 max-w-lg">
-                {itemTextCol}
-            </article>
-        )
-
-    return content
+  return content;
 }
